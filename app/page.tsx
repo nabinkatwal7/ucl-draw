946cab48b18742a506cee00bbfd81ae8ae50b515
Nabin Katwal <nabinkatwal2000@gmail.com>
2024-10-25 13:47:18 +0545

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <h3 className="capitalize"> hello k xa timor khabar dfdsfsdf dsf</h3>
-    </div>
-  );
+  return <div className="">asdf</div>;
 }
